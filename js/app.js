841f30f354749017f594fc1778a26ac899d015d1
Rocky <mymac@MyMacs-MacBook-Pro.local>
2021-04-26 14:56:14 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,60 @@
+//defines the units
+
+let zone1 = document.querySelector(".zone1");
+let zone2 = document.querySelector(".zone2");
+let flightLine = document.querySelector(".flightline");
+let s7 = document.querySelector(".s7");
+let s8 = document.querySelector(".s8");
+let puertoGate = document.querySelector(".puertogate");
+let rotaGate = document.querySelector(".rotagate");
+let pier1 = document.querySelector(".pier1");
+let r1 = document.querySelector(".r1");
+
+
+
+//removes z1 after clicked
+zone1.addEventListener("click", function(){
+    zone1.style.visibility = "hidden";    
+});
+
+//removes z2 after clicked
+zone2.addEventListener("click", function(){
+    zone2.style.visibility = "hidden";  
+});
+
+//removes flightline after clicked
+flightLine.addEventListener("click", function(){
+    flightLine.style.visibility = "hidden";  
+});
+
+//removes s7 after clicked
+s7.addEventListener("click", function(){
+    s7.style.visibility = "hidden";  
+});
+
+//removes s8 after clicked
+s8.addEventListener("click", function(){
+    s8.style.visibility = "hidden";  
+});
+
+//removes puertogate after clicked
+puertoGate.addEventListener("click", function(){
+    puertoGate.style.visibility = "hidden";  
+});
+
+//removes rotagate after clicked
+rotaGate.addEventListener("click", function(){
+    rotaGate.style.visibility = "hidden";  
+});
+
+//removes pier1 after clicked
+pier1.addEventListener("click", function(){
+    pier1.style.visibility = "hidden";  
+});
+
+//removes r1 after clicked
+r1.addEventListener("click", function(){
+    r1.style.visibility = "hidden";  
+});
+
+
